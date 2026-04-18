2a2fccd538d6bde81e8cfc9202175d199e04bfe4
daryihsan <ihsandary2@gmail.com>
2026-04-18 15:40:26 +0700

    revisi

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -53,12 +53,23 @@ def create_bycity_df(df, entity="customer"):
 
     return bycity_df.sort_values(by="count", ascending=False).head(10)
 
-pwd = os.path.dirname(__file__)
+def create_rfm_df(df):
+    rfm_df = df.groupby(by="customer_unique_id", as_index=False).agg({
+        "order_purchase_timestamp": "max",
+        "order_id": "nunique",
+        "price": "sum"
+    })
+    rfm_df.columns = ["customer_unique_id", "max_order_timestamp", "frequency", "monetary"]
+    
+    # Hitung recency dinamis berdasarkan filter
+    recent_date = df["order_purchase_timestamp"].max().date() + pd.Timedelta(days=1)
+    rfm_df["recency"] = rfm_df["max_order_timestamp"].dt.date.apply(lambda x: (recent_date - x).days)
+    return rfm_df
+
 
 # Load data yang sudah diekspor dari notebook
+pwd = os.path.dirname(__file__)
 main_df = pd.read_csv(os.path.join(pwd, "main_data.csv"))
-rfm_df = pd.read_csv(os.path.join(pwd, "rfm_data.csv"))
-
 main_df["order_purchase_timestamp"] = pd.to_datetime(main_df["order_purchase_timestamp"])
 
 # Sidebar untuk filter tanggal
@@ -92,6 +103,7 @@ customer_state_df = create_bystate_df(main_df_filtered, entity="customer")
 seller_state_df = create_bystate_df(main_df_filtered, entity="seller")
 customer_city_df = create_bycity_df(main_df_filtered, entity="customer")
 seller_city_df = create_bycity_df(main_df_filtered, entity="seller")
+rfm_df = create_rfm_df(main_df_filtered)
 
 # Main dashboard
 st.header('🛍️ E-Commerce Dashboard')
@@ -122,6 +134,8 @@ ax.tick_params(axis='y', labelsize=15)
 ax.tick_params(axis='x', labelsize=15)
 st.pyplot(fig)
 
+colors_5 = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+colors_10 = ["#72BCD4"] + ["#D3D3D3"] * 9
 
 # Produk dan Kategori Terbaik & Terburuk
 st.subheader("Best & Worst Performing Categories")
@@ -131,16 +145,12 @@ top_5_categories = category_sales_df.sort_values(by="total_sales", ascending=Fal
 bottom_5_categories = category_sales_df.sort_values(by="total_sales", ascending=True).head(5)
 
 # Chart kiri best performing
-sns.barplot(x="total_sales", y="product_category_name", data=top_5_categories, ax=ax[0], palette="Greens_r", hue="product_category_name", legend=False)
-ax[0].set_ylabel(None)
-ax[0].set_xlabel(None)
+sns.barplot(x="total_sales", y="product_category_name", data=top_5_categories, ax=ax[0], palette=colors_5, hue="product_category_name", legend=False)
 ax[0].set_title("Best Performing Categories", loc="center", fontsize=20)
 ax[0].tick_params(axis='y', labelsize=15)
 
 # Chart kanan worst performing
-sns.barplot(x="total_sales", y="product_category_name", data=bottom_5_categories, ax=ax[1], palette="Reds_r", hue="product_category_name", legend=False)
-ax[1].set_ylabel(None)
-ax[1].set_xlabel(None)
+sns.barplot(x="total_sales", y="product_category_name", data=bottom_5_categories, ax=ax[1], palette=colors_5, hue="product_category_name", legend=False)
 ax[1].invert_xaxis()
 ax[1].yaxis.set_label_position("right")
 ax[1].yaxis.tick_right()
@@ -156,16 +166,12 @@ top_5_products = product_sales_df.sort_values(by="total_sales", ascending=False)
 bottom_5_products = product_sales_df.sort_values(by="total_sales", ascending=True).head(5)
 
 # Chart kiri best performing
-sns.barplot(x="total_sales", y="product_id", data=top_5_products, ax=ax[0], palette="Greens_r", hue="product_id", legend=False)
-ax[0].set_ylabel(None)
-ax[0].set_xlabel(None)
+sns.barplot(x="total_sales", y="product_id", data=top_5_products, ax=ax[0], palette=colors_5, hue="product_id", legend=False)
 ax[0].set_title("Best Performing Products", loc="center", fontsize=20)
 ax[0].tick_params(axis='y', labelsize=15)
 
 # Chart kanan worst performing
-sns.barplot(x="total_sales", y="product_id", data=bottom_5_products, ax=ax[1], palette="Reds_r", hue="product_id", legend=False)
-ax[1].set_ylabel(None)
-ax[1].set_xlabel(None)
+sns.barplot(x="total_sales", y="product_id", data=bottom_5_products, ax=ax[1], palette=colors_5, hue="product_id", legend=False)
 ax[1].invert_xaxis()
 ax[1].yaxis.set_label_position("right")
 ax[1].yaxis.tick_right()
@@ -180,16 +186,12 @@ st.subheader("Demographics by State")
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 8))
 
 # Chart kiri customers
-sns.barplot(x="count", y="customer_state", data=customer_state_df, ax=ax[0], palette="Blues_r", hue="customer_state", legend=False)
-ax[0].set_ylabel(None)
-ax[0].set_xlabel(None)
+sns.barplot(x="count", y="customer_state", data=customer_state_df, ax=ax[0], palette=colors_10, hue="customer_state", legend=False)
 ax[0].set_title("Demography State of Customers", loc="center", fontsize=20)
 ax[0].tick_params(axis='y', labelsize=15)
 
 # Chart kanan sellers
-sns.barplot(x="count", y="seller_state", data=seller_state_df, ax=ax[1], palette="Oranges_r", hue="seller_state", legend=False)
-ax[1].set_ylabel(None)
-ax[1].set_xlabel(None)
+sns.barplot(x="count", y="seller_state", data=seller_state_df, ax=ax[1], palette=colors_10, hue="seller_state", legend=False)
 ax[1].set_title("Demography State of Sellers", loc="center", fontsize=20)
 ax[1].tick_params(axis='y', labelsize=15)
 
@@ -200,57 +202,40 @@ st.subheader("Demographics by City")
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 8))
 
 # Chart kiri customers
-sns.barplot(x="count", y="customer_city", data=customer_city_df, ax=ax[0], palette="Blues_r", hue="customer_city", legend=False)
-ax[0].set_ylabel(None)
-ax[0].set_xlabel(None)
+sns.barplot(x="count", y="customer_city", data=customer_city_df, ax=ax[0], palette=colors_10, hue="customer_city", legend=False)
 ax[0].set_title("Demography City of Customers", loc="center", fontsize=20)
 ax[0].tick_params(axis='y', labelsize=15)
 
 # Chart kanan sellers
-sns.barplot(x="count", y="seller_city", data=seller_city_df, ax=ax[1], palette="Oranges_r", hue="seller_city", legend=False)
-ax[1].set_ylabel(None)
-ax[1].set_xlabel(None)
+sns.barplot(x="count", y="seller_city", data=seller_city_df, ax=ax[1], palette=colors_10, hue="seller_city", legend=False)
 ax[1].set_title("Demography City of Sellers", loc="center", fontsize=20)
 ax[1].tick_params(axis='y', labelsize=15)
 
 st.pyplot(fig)
 
 # RFM Analysis
-# RFM tidak difilter berdasarkan sidebar karena RFM mewakili seluruh histori pelanggan
 st.subheader("Best Customers Based on RFM Parameters (customer_unique_id)")
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    avg_recency = round(rfm_df.recency.mean(), 1)
-    st.metric("Average Recency (days)", value=avg_recency)
-
+    st.metric("Average Recency (days)", value=round(rfm_df.recency.mean(), 1))
 with col2:
-    avg_frequency = round(rfm_df.frequency.mean(), 2)
-    st.metric("Average Frequency", value=avg_frequency)
-
+    st.metric("Average Frequency", value=round(rfm_df.frequency.mean(), 2))
 with col3:
-    avg_monetary = rfm_df.monetary.mean()
-    st.metric("Average Monetary", value=format_currency(avg_monetary, "BRL", locale='pt_BR'))
+    st.metric("Average Monetary", value=format_currency(rfm_df.monetary.mean(), "BRL", locale='pt_BR'))
 
 fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(30, 10))
-colors = ["#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4"]
 
-sns.barplot(y="recency", x="customer_unique_id", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0], hue="customer_unique_id", legend=False)
-ax[0].set_ylabel(None)
-ax[0].set_xlabel(None)
+sns.barplot(y="recency", x="customer_unique_id", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors_5, ax=ax[0], hue="customer_unique_id", legend=False)
 ax[0].set_title("By Recency (days)", loc="center", fontsize=18)
 ax[0].tick_params(axis='x', labelsize=10, rotation=45)
 
-sns.barplot(y="frequency", x="customer_unique_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1], hue="customer_unique_id", legend=False)
-ax[1].set_ylabel(None)
-ax[1].set_xlabel(None)
+sns.barplot(y="frequency", x="customer_unique_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors_5, ax=ax[1], hue="customer_unique_id", legend=False)
 ax[1].set_title("By Frequency", loc="center", fontsize=18)
 ax[1].tick_params(axis='x', labelsize=10, rotation=45)
 
-sns.barplot(y="monetary", x="customer_unique_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2], hue="customer_unique_id", legend=False)
-ax[2].set_ylabel(None)
-ax[2].set_xlabel(None)
+sns.barplot(y="monetary", x="customer_unique_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors_5, ax=ax[2], hue="customer_unique_id", legend=False)
 ax[2].set_title("By Monetary", loc="center", fontsize=18)
 ax[2].tick_params(axis='x', labelsize=10, rotation=45)
 
